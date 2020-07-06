97a764a37cfed18904b188249a51d4d0f7ec7fc5
Chidume Nnamdi <kurtwanger5@gmail.com>
2020-07-06 07:42:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const helmet = require('helmet')
 const path = require('path')
 const axios = require("axios")
 
-const log = console.log
+// const log = console.log
 const app = express()
 
-let port = 4000 || process.env.PORT
+let port = process.env.PORT || 4000 
 
 /** set up middlewares */
 app.use((req, res, next) => {
@@ -22,12 +22,10 @@ app.use(helmet())
 
 app.get("/proxy", (req, res) => {
     var url = req.param('proxyUrl')
-    log(url)
     axios({
         url,
         method: "GET"
     }).then(response => {
-        log(response.data)
         res.json(response.data);
     }).catch(err => {
         res.status(500).json({ type: 'error', message: err.toString() });
